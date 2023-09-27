4f704fccf29cdaa74ad149586bbd32be2cd39ece
wookey(안형욱) <wookey.ahn@bunjang.co.kr>
2023-09-27 16:57:02 +0900

    security

---
+++ src/main/java/com/example/domain/response/UserResponse.java
@@ -0,0 +1,48 @@
+package com.example.domain.response;
+
+import com.example.domain.entity.User;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Getter
+public class UserResponse implements UserDetails {
+    private final String username;
+    private final String password;
+    private final String role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public static UserResponse of(User user) {
+        return new UserResponse(user.getUsername(), user.getPassword(), user.getRole());
+    }
+}
