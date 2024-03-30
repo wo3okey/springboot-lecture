0b4133c85fde693041a9d07b46846958399b9e58
wookey <anbriaao@naver.com>
2024-03-30 17:34:56 +0900

    jpa-lock1

---
+++ src/main/java/com/example/common/Timer.java
@@ -0,0 +1,38 @@
+package com.example.common;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+
+@Target({ElementType.TYPE, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Timer {
+}
+
+@Aspect
+@Component
+class TimerImpl {
+    @Pointcut("@annotation(com.example.common.Timer)")
+    private void timer() {
+    }
+
+    @Around("timer()")
+    public void around(ProceedingJoinPoint joinPoint) throws Throwable {
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+
+        joinPoint.proceed();
+
+        stopWatch.stop();
+        System.out.println("total time : " + stopWatch.getTotalTimeSeconds());
+    }
+}
