2f28215da8e87acaef62c068cbc798dc568c17f5
wookey <anbriaao@naver.com>
2024-05-13 21:36:30 +0900

    boot3

--- src/main/java/com/example/domain/entity/Actor.java
+++ src/main/java/com/example/domain/entity/Actor.java
@@ -3,14 +3,14 @@ package com.example.domain.entity;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "actor")
